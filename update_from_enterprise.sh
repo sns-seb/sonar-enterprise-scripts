13bf368b174e94faba3c6f73413e72889ed7b764
Sébastien Lesaint <sebastien.lesaint@sonarsource.com>
2018-03-16 17:13:53 +0100

    add script to update sonarqube from sonar-enterprise

---
+++ update_from_enterprise.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+##############################################################################################
+##
+## This script is intended to be run on a clone of repository SonarSource/sonarqube.
+##
+##############################################################################################
+
+
+set -euo pipefail
+
+function info() {
+  local MESSAGE="$1"
+  echo "[INFO] ${MESSAGE}"
+}
+
+ENTERPRISE_REPO="git@github.com:SonarSource/sonar-enterprise.git"
+
+info "updating sonarqube master from sonar-enterprise public_master..."
+
+if [ "$(git remote -v | grep "enterprise" || true)" = "" ]; then
+  git remote add enterprise "${ENTERPRISE_REPO}"
+fi
+
+git fetch --no-tags "enterprise" "public_master" 
+
+git checkout "master"
+git merge --ff-only "enterprise/public_master"
+
+info "done"
